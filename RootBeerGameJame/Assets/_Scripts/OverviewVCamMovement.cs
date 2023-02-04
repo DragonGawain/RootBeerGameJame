3eee635254b3c0b65228dc99811106d2afe644ec
Craig <Craig@craigs-mbp.wireless.concordia.ca>
2023-02-04 17:27:33 -0500

    Set up camera moving

---
+++ RootBeerGameJame/Assets/_Scripts/OverviewVCamMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class OverviewVCamMovement : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam2;
+    public CinemachineTrackedDolly dolly;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dolly = vcam2.GetCinemachineComponent<CinemachineTrackedDolly>();
+        dolly.m_PathPosition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dolly.m_PathPosition = dolly.m_PathPosition + 0.005f; //set speed to 0.05  
+
+        if (dolly.m_PathPosition >= 2) {
+            vcam2.VirtualCameraGameObject.SetActive(false);
+        }
+    }
+}
