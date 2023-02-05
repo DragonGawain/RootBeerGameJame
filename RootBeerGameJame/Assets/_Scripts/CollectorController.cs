3b10072d6f9adb587a1b74f3930e136bbaaf42e8
Lorenzo <61562227+Lorenzo767@users.noreply.github.com>
2023-02-04 19:24:02 -0500

    coin added

---
+++ RootBeerGameJame/Assets/_Scripts/CollectorController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CollectorController : MonoBehaviour
+{
+    //manages points
+    //checks if you collected something and does appropriate behaviour 
+    [SerializeField] TMP_Text pointsDisplay;
+    private int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Collectables"))
+        {
+            if(other.gameObject.tag == "25cent")
+            {
+                points += 25;
+                pointsDisplay.text = points.ToString();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
